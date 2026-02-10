a051ad02b5dcc43a9a63e64441502b19b47608af
Gabriel Lentine <gabrielmarinholentine@gmail.com>
2026-02-10 19:15:03 -0300

    feat: aula sobre operadores aritméticos e lógicos

---
+++ 01_Operadores/op_aritmeticos.py
@@ -0,0 +1,16 @@
+# Operadores Aritméticos em Python: soma, subtração, multiplicação e divisão
+
+print(6+4)
+# retorna a soma = 10
+
+print(6-4)
+# retorna a subtração = 2
+
+print(6*4)
+# retorna a multiplicação =24
+
+print(6/4)
+# retorna a divisão completa (com decimais, se houver) = 1.5
+
+print(6//4)
+# retorna a divisão inteira (sem decimais) = 1
---
+++ 01_Operadores/op_logicos.py
@@ -0,0 +1,19 @@
+# Operadores Lógicos em Python: TRUE or FALSE (maior, maior igual, menor, menor igual, igual, diferente)
+
+print(8 > 4)
+# true (8 é maior que 4)
+
+print(4 > 8)
+# false (4 não é maior que 8)
+
+print(4 >= 4)
+# true (4 é maior ou igual a 4)
+
+print(5 <= 4)
+# false (5 não é menor ou igual a 4)
+
+print(8 == 8)
+# true (8 é igual a 8)
+
+print(10 != 11)
+# true (10 é diferente de 11)
