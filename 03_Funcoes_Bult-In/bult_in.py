8f28311dca9baea89e16fa9c2cd1e2551b675c8d
Gabriel Lentine <gabrielmarinholentine@gmail.com>
2026-02-10 19:56:49 -0300

    feat: aula sobre funções built-in

---
+++ 03_Funcoes_Bult-In/bult_in.py
@@ -0,0 +1,25 @@
+# São funções que já vêm prontas para uso no Python: no total, existem 68 funções built-in
+
+print(min(3, 5, 29, 40, 1, 0, 42))
+# retorna o menor valor de uma sequência de números = 0
+
+print(max(3, 5, 29, 40, 1, 0, 42))
+# retorna o maior valor de uma sequência de números = 42
+
+print(abs(-10))
+# retorna o valor absoluto de um número = 10
+
+print(sum([3, 20, 5]))
+# retorna a soma de uma sequência de números = 28
+
+print(pow(2, 3))
+# retorna a potência de uma base (2) elevada ao expoente (3) = 8
+
+print("usando print")
+# retorna um valor na saída
+
+print(input("Digite um valor aqui: "))
+# lê um valor digitado no teclado pelo usuário
+
+help(max)
+# ajuda a entender como uma função funciona
