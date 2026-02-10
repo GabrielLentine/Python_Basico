95b92f9b9162094e6fb2cdbacb57f6b5530c320c
Gabriel Lentine <gabrielmarinholentine@gmail.com>
2026-02-10 19:25:13 -0300

    feat: aula sobre variáveis

---
+++ 02_Variáveis/variaveis.py
@@ -0,0 +1,24 @@
+# Variáveis são lugares de memória que irão armazenar um dado
+
+# <nome_variavel> = <valor_variavel>
+nome_pessoa = "Gabriel Lentine"
+
+# Seu nome pode ter letras maiúsculas, minúsculas e underscores
+nomePessoa = "Gabriel Lentine"
+_reservado = "Reservado"
+
+# Não pode começar com um número
+# 1nome_pessoa = "Gabriel Lentine" está errado e nem roda
+
+
+# É preferível colcar nomes que sejam descritivos e fáceis de entender
+# Evite: a = 10
+# Prefira: valor_a = 10
+
+# Para nomes compostos, usar underscore ou letra maiúscula para separar as palavras
+nome_pessoa = "Gabriel Lentine"
+NomePessoa = "Gabriel Lentine"
+nomePessoa = "Gabriel Lentine"
+
+# evitar nomes genérios e/ou muito longos
+nome_pessoa_completo_com_idade = "Gabriel Lentine 19 anos"
